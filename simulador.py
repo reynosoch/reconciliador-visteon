68fa9c1ce900490c160b186a4afd1309fb49b94a
reynosoch <javireynoso21@gmail.com>
2026-09-22 11:55:42 -0600

    base datos prueba supabase corregida

--- simulador.py
+++ simulador.py
@@ -1,6 +1,10 @@
 import time
 import random
 import requests
+import warnings
+from urllib3.exceptions import InsecureRequestWarning
+# Apagar el warning de SSL (solo porque tu proxy corporativo intercepta el certificado)
+warnings.simplefilter('ignore', InsecureRequestWarning)
 # Credenciales de Supabase
 URL = "https://uukhwkywmnarcfruerpp.supabase.co/rest/v1/escaneos_4wall"
 API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1a2h3a3l3bW5hcmNmcnVlcnBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwODI4OTgsImV4cCI6MjEwNTY1ODg5OH0.ezApb_e8_Q-_yvxmZL4b3skmmMXoJaya4oupSzPz3Vc"
@@ -25,7 +29,7 @@ while True:
                "cantidad": cantidad,
                "area_escaneo": area
            }
-           response = requests.post(URL, json=payload, headers=HEADERS)
+           response = requests.post(URL, json=payload, headers=HEADERS, verify=False)
            icono = "👻" if parte.startswith("P7") else ("⚠️" if "VPRLXF" in parte else "📦")
            if response.status_code == 201:
                print(f"{icono} NUBE: {cantidad} pz de {parte} en {area}")
